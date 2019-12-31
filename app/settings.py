bd03241831829f0ab2768be62a518fd523b02581
Robert McCormick <robertmccormick@Roberts-MBP.lan>
2019-12-31 13:27:31 +0000

    Updated app to work on heroku web and locally

--- app/settings.py
+++ app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,3 +123,6 @@ STATIC_URL = '/static/'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+# Set up database for Heroku
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
