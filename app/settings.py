9283adb9868178180dcf39ffdbe95af5b1507138
Robert McCormick <robertmccormick@Roberts-MBP.lan>
2020-01-03 18:24:32 +0000

    Added djangorestframework-api-key package and is working with chatbot api key

--- app/settings.py
+++ app/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_api_key',
     'core',
     'cbjobsdata',
     'whitenoise.runserver_nostatic',
--- cbjobsdata/views.py
+++ cbjobsdata/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
 from rest_framework.generics import get_object_or_404
 
+from rest_framework_api_key.permissions import HasAPIKey
+
 from core.models import CbJobsData, Company
 
 from cbjobsdata.serializers import CbJobsDataCreateSerializer
@@ -10,6 +12,7 @@ class CbJobsDataCreateView(generics.CreateAPIView):
     """View to create new job data objects in the database"""
     queryset = CbJobsData.objects.all()
     serializer_class = CbJobsDataCreateSerializer
+    permission_classes = (HasAPIKey,)
 
     def perform_create(self, serializer):
         """Over-rides the default perform_create as using ForeignKey"""
